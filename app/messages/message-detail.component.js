b5e7a8053b8afca4ff020e40fa4307bc9f1077b1
Andrew Niekamp <andrew.f.niekamp@gmail.com>
2018-01-26 07:13:11 -0500

    Set initial select value on controller for default view

--- app/messages/message-detail.component.js
+++ app/messages/message-detail.component.js
@@ -9,6 +9,7 @@
         templateUrl: '/app/messages/message-detail.component.html',
         controllerAs: 'vm',
         controller: function() {
+            this.logOption = 'Meta';
             this.options = ['Meta', 'Detail'];
         }
     });
